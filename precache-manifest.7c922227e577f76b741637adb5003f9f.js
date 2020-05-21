8c1d7bb9bfd627daa98f110420bffe4f5205efa2
ChristophSiegenthaler <csi@gmx.ch>
2020-05-21 22:09:41 +0200

    Updates

---
+++ precache-manifest.7c922227e577f76b741637adb5003f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab9ade03a72a8aa18f45eec583ecf08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bdb432205f922a5fb04",
+    "url": "/static/css/2.826c934e.chunk.css"
+  },
+  {
+    "revision": "e8166fa13b0351e44126",
+    "url": "/static/css/main.043bb928.chunk.css"
+  },
+  {
+    "revision": "5bdb432205f922a5fb04",
+    "url": "/static/js/2.472337f8.chunk.js"
+  },
+  {
+    "revision": "e8166fa13b0351e44126",
+    "url": "/static/js/main.5a9495c9.chunk.js"
+  },
+  {
+    "revision": "bba48fb066527e8b83a1",
+    "url": "/static/js/runtime-main.48c9294d.js"
+  }
+]);
